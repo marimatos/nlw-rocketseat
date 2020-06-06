bbc7374baa5caa96e18b442aaf80c0318c92bfbb
marinamatos <marina.msp@gmail.com>
2020-06-06 16:17:20 -0300

    Upload de arquivos

---
+++ server/src/routes.ts
@@ -0,0 +1,15 @@
+import express from 'express';
+
+import PointsController from './controllers/PointsController';
+import ItemsController from './controllers/ItemsController';
+
+const routes = express.Router();
+const pointsController = new PointsController();
+const itemsController = new ItemsController();
+
+routes.get('/items', itemsController.index);
+routes.post('/point', pointsController.create);
+routes.get('/point', pointsController.index);
+routes.get('/point/:id', pointsController.show);
+
+export default routes;
